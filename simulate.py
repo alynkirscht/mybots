a92b3deb90b4be27f00912905647f222d7e06110
alynkirscht <akirscht@uvm.edu>
2022-01-27 12:49:00 -0500

    Added the box to simulation and commented program

--- simulate.py
+++ simulate.py
@@ -1,9 +1,19 @@
+#Alyn Kirsch Tornell
+#This program simulates the world robots will live in
 import pybullet as p
 import time
 
+#Create objects that handles physics and draws results to GUI
 physicsClient = p.connect(p.GUI)
-for i in range(500):
+
+#Reads in the world described in box.sdf
+p.loadSDF("box.sdf")
+
+#The for loop is used to slow thingd down
+for i in range(1000):
+    #Steps physics inside the world
     p.stepSimulation()
+    #Slows things down by 1/60 second of each iteration of the loop
     time.sleep(1/60)
     print (i)
 p.disconnect()
