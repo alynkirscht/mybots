4a20cc1c6a1506147fcf1b11128554e6ba1fd596
akirscht <akirscht@uvm.edu>
2022-02-08 15:29:28 -0500

    Add position variables

--- generate.py
+++ generate.py
@@ -5,14 +5,17 @@ import pyrosim.pyrosim as pyrosim
 
 #Variables
 length = 1
-width = 2
-height = 3
+width = 1
+height = 1
+x = 0
+y = 0
+z = .5
 
 #Tell pyrosim where info about world (box) should be stored
 pyrosim.Start_SDF("box.sdf")
 
 #Store a box with specific position and size
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[width,length,height])
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[width,length,height])
 
 #Close sdf file
 pyrosim.End()
