d69dfeb66401cd984ddf1462ab61d551b60c8682
akirscht <akirscht@uvm.edu>
2022-02-08 15:07:59 -0500

    Add measurement variables to box

--- generate.py
+++ generate.py
@@ -3,11 +3,16 @@
 
 import pyrosim.pyrosim as pyrosim
 
+#Variables
+length = 1
+width = 2
+height = 3
+
 #Tell pyrosim where info about world (box) should be stored
 pyrosim.Start_SDF("box.sdf")
 
 #Store a box with specific position and size
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[width,length,height])
 
 #Close sdf file
 pyrosim.End()
