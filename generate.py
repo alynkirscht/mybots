2240a966d3d720f210da5f03f17dca14008f38fc
akirscht <akirscht@uvm.edu>
2022-02-08 15:35:48 -0500

    Add another box

--- generate.py
+++ generate.py
@@ -12,10 +12,13 @@ y = 0
 z = .5
 
 #Tell pyrosim where info about world (box) should be stored
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 #Store a box with specific position and size
 pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[width,length,height])
+
+#Store a box with specific position and size
+pyrosim.Send_Cube(name="Box2", pos=[x+1,y,z+1] , size=[width,length,height])
 
 #Close sdf file
 pyrosim.End()
