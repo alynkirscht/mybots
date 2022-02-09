6bfa358fd960f836197420e3fc9cb4b392b25e38
akirscht <akirscht@uvm.edu>
2022-02-08 22:23:20 -0500

    Add sensor

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 
 #Create objects that handles physics and draws results to GUI
 physicsClient = p.connect(p.GUI)
@@ -22,10 +23,15 @@ robotId = p.loadURDF("body.urdf")
 #Reads in the world described in world.sdf
 p.loadSDF("world.sdf")
 
+#Set up sensors
+pyrosim.Prepare_To_Simulate(robotId)
+
 #The for loop is used to slow things down
 for i in range(1000):
     #Steps physics inside the world
     p.stepSimulation()
+    #Add sensor to BackLeg link
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     #Slows things down by 1/60 second of each iteration of the loop
     time.sleep(1/60)
     print (i)
