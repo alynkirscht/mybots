6d5d53eb89a329ebb22b600edd89be5fe617d5fc
akirscht <akirscht@uvm.edu>
2022-02-08 23:20:05 -0500

    Store sensor values in numpy vector

--- simulate.py
+++ simulate.py
@@ -4,6 +4,7 @@ import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 #Create objects that handles physics and draws results to GUI
 physicsClient = p.connect(p.GUI)
@@ -26,13 +27,18 @@ p.loadSDF("world.sdf")
 #Set up sensors
 pyrosim.Prepare_To_Simulate(robotId)
 
+#Numpy vector with same amount of zeros as loop
+backLegSensorsValues = numpy.zeros(1000)
+
 #The for loop is used to slow things down
 for i in range(1000):
     #Steps physics inside the world
     p.stepSimulation()
     #Add sensor to BackLeg link
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    backLegSensorsValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     #Slows things down by 1/60 second of each iteration of the loop
     time.sleep(1/60)
-    print (i)
+    #print (i)
 p.disconnect()
+
+print(backLegSensorsValues)
