ad2ac53c170a7ca37331ddf67002f72feff207bf
akirscht <akirscht@uvm.edu>
2022-02-09 00:16:33 -0500

    Add front leg sensor data

--- analyze.py
+++ analyze.py
@@ -5,8 +5,10 @@ import numpy
 import matplotlib.pyplot
 
 #Get sensor data from file
-backLegSensorValues = numpy.load("data/backLegSensorsValues.npy")
+backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
+frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
 
 #Plot analyzed data
 matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.plot(frontLegSensorValues)
 matplotlib.pyplot.show()
