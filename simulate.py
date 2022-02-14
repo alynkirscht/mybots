bc0776057377116f8e7b5f377178ac1ed8faa0f8
akirscht <akirscht@uvm.edu>
2022-02-14 11:01:52 -0500

    Add open-loop control movement

--- simulate.py
+++ simulate.py
@@ -5,10 +5,16 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
-import math
 import random
 
-pi = math.pi
+pi = numpy.pi
+amplitude_BackLeg = pi/4
+frequency_BackLeg = 10
+phaseOffset_BackLeg = pi/8
+
+amplitude_FrontLeg = pi/4
+frequency_FrontLeg = 10
+phaseOffset_FrontLeg = 0
 
 #Create objects that handles physics and draws results to GUI
 physicsClient = p.connect(p.GUI)
@@ -31,11 +37,24 @@ p.loadSDF("world.sdf")
 #Set up sensors
 pyrosim.Prepare_To_Simulate(robotId)
 
-#Numpy backLeg sensor vector
+#backLeg sensor vector
 backLegSensorValues = numpy.zeros(1000)
 
-#Numpy frontLeg sensor vector
+#frontLeg sensor vector
 frontLegSensorValues = numpy.zeros(1000)
+
+#targetAngles vector
+targetAngles_BackLeg = numpy.zeros(1000)
+targetAngles_BackLeg = amplitude_BackLeg*numpy.sin(frequency_BackLeg * numpy.linspace(0,2*pi,1000) + phaseOffset_BackLeg)
+targetAngles_FrontLeg = numpy.zeros(1000)
+targetAngles_FrontLeg = amplitude_FrontLeg*numpy.sin(frequency_FrontLeg * numpy.linspace(0,2*pi,1000) + phaseOffset_FrontLeg)
+
+#Save sensor and motor data to file
+#numpy.save("data\\backLegSensorValues.npy", backLegSensorValues)
+#numpy.save("data\\frontLegSensorValues.npy", frontLegSensorValues)
+#numpy.save("data\\targetAnglesBackLeg.npy",targetAngles_BackLeg)
+#numpy.save("data\\targetAnglesFrontLeg.npy",targetAngles_FrontLeg)
+#exit()
 
 #The for loop is used to slow things down
 for i in range(1000):
@@ -48,16 +67,17 @@ for i in range(1000):
     #Add motor for BackLeg and FrontLeg to torso joints
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName= "Torso_BackLeg",
                                 controlMode= p.POSITION_CONTROL,
-                                targetPosition= random.uniform(-pi/2,pi/2),
-                                maxForce = 35)
+                                targetPosition= targetAngles_BackLeg[i],
+                                maxForce = 15)
     pyrosim.Set_Motor_For_Joint(bodyIndex = robotId, jointName= "Torso_FrontLeg",
                                 controlMode= p.POSITION_CONTROL,
-                                targetPosition= random.uniform(-pi/2,pi/2),
-                                maxForce = 35)
+                                targetPosition= targetAngles_FrontLeg[i],
+                                maxForce = 15)
     #Slows things down by 1/60 second of each iteration of the loop
     time.sleep(1/60)
-    print (i)
-#Save sensor data to file
-numpy.save("data\\backLegSensorValues.npy", backLegSensorValues)
-numpy.save("data\\frontLegSensorValues.npy", frontLegSensorValues)
+    
 p.disconnect()
+
+
+
+
