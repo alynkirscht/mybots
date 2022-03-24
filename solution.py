cfdf1a86f458586112ca634d7b457f25b7bf2c55
akirscht <akirscht@uvm.edu>
2022-03-24 03:25:36 -0400

    Evaluate

--- solution.py
+++ solution.py
@@ -1,4 +1,7 @@
 import numpy
+import pyrosim.pyrosim as pyrosim
+import random
+import os
 
 class SOLUTION:
     def __init__(self):
@@ -8,3 +11,51 @@ class SOLUTION:
         
         self.weights = self.weights * 2 -1
 
+    def Evaluate(self):
+        self.Create_World()
+        self.Generate_Body()
+        self.Generate_Brain()
+        os.system("python3 simulate.py")
+
+    def Create_World(self):
+        
+        pyrosim.Start_SDF("world.sdf")
+
+        pyrosim.Send_Cube(name="Box", pos=[-2,2,.5], size=[1, 1, 1])
+
+        pyrosim.End()
+
+    def Generate_Body(self):
+
+        pyrosim.Start_URDF("body.urdf")
+        
+        pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[1, 1, 1])
+        
+        pyrosim.Send_Joint( name = "Torso_BackLeg", parent = "Torso", child = "BackLeg",
+                            type = "revolute", position = [1,0,1])
+        pyrosim.Send_Cube(name="BackLeg", pos=[-.5,0,-.5], size=[1, 1, 1])
+
+        pyrosim.Send_Joint( name = "Torso_FrontLeg", parent = "Torso", child = "FrontLeg",
+                            type = "revolute", position = [2,0,1])
+        pyrosim.Send_Cube( name="FrontLeg", pos=[.5,0,-.5], size=[1,1,1])
+
+        pyrosim.End()
+
+    def Generate_Brain(self):
+
+        pyrosim.Start_NeuralNetwork("brain.nndf")
+
+        pyrosim.Send_Sensor_Neuron(name = 0, linkName = "Torso")
+        pyrosim.Send_Sensor_Neuron(name = 1, linkName = "BackLeg")
+        pyrosim.Send_Sensor_Neuron(name = 2, linkName = "FrontLeg")
+
+        pyrosim.Send_Motor_Neuron( name = 3, jointName = "Torso_BackLeg")
+        pyrosim.Send_Motor_Neuron( name = 4, jointName = "Torso_FrontLeg")
+
+        for currentRow in range(3):
+            for currentColumn in range(2):
+                pyrosim.Send_Synapse(sourceNeuronName = currentRow ,
+                                     targetNeuronName = currentColumn + 3 ,
+                                     weight = self.weights[currentRow][currentColumn])
+        
+        pyrosim.End()
