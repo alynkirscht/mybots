bb496b5d8cce53ad87461084704c33e486986929
alynkirscht <92424874+alynkirscht@users.noreply.github.com>
2023-05-08 15:43:41 -0400

    Mann Whitney U test

--- stats.py
+++ stats.py
@@ -1,17 +1,15 @@
-import pandas as pd
-from scipy.stats import spearmanr
+from scipy.stats import mannwhitneyu
 
-# read the data from the CSV files
-x_data = pd.read_csv('data\\angleAxisRandom.csv')
-y_data = pd.read_csv('data\\angleAxis20Gen.csv')
+seg3_gen1 = [0.1015225571, 0.03959743532, 0.4841933672, 1.226476481, 0.05380792647, 0.02486603242, 0.06428163675, 0.5576403075, -0.3535230612]
+seg4_gen1 = [-1.288243034, -1.462333419, -1.791035763, -2.291196377, -1.620514932, -0.5507917839, -1.514052131, -2.144056488, -0.9964500957, -1.203322232, -1.288243034]
 
-for i in range(6):
-    if (i == 5):
-        corr, pval = spearmanr(x_data['Fitness'], y_data['Fitness'])
-    else:
-        corr, pval = spearmanr(x_data['Joint ' + str(i+1)], y_data['Joint ' + str(i+1)])
-    print(f"Spearman correlation coefficient between column {i+1} in random snakes and column {i+1} in  30 gen evolved snakes: {corr:.3f}")
-    print(f"p-value: {pval:.3f}")
+seg3_gen200 = [-0.994039762, -1.023867135, -1.60183642, -1.123495789, -1.101276431, -0.7256849013, -1.02768712, -1.098612096, -0.3395070353, -0.8133859422]
+seg4_gen200 = [-0.9684575587, -1.145829145, -0.8828304544, -1.526075062, -0.9375034762, -1.00397796, -1.34155856, -0.1054697576, -0.6819930568, -1.446478347]
 
 
 
+
+U1, p = mannwhitneyu(seg3_gen1, seg4_gen1)
+print("Parent generation 1: U1 = " + str(U1) + " p = " + str(p))
+U1, p = mannwhitneyu(seg3_gen200, seg4_gen200)
+print("Parent generation 200: U1 = " + str(U1) + " p = " + str(p))
