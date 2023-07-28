8505f57ee1c140e8b40dde29b260267394e8c671
akirscht <akirscht@uvm.edu>
2023-07-28 11:07:01 -0400

    Add mass as a parameter

--- generate.py
+++ generate.py
@@ -2,13 +2,41 @@
 #This program generates one link
 
 import pyrosim.pyrosim as pyrosim
+#Create_World creates the simulated world, with a box in it
+def Create_World():
+    
+    #Tell pyrosim where info about world (world) should be stored
+    pyrosim.Start_SDF("world.sdf")
 
-#Tell pyrosim where info about world (box) should be stored
-pyrosim.Start_SDF("box.sdf")
+    #Store a box with specific position and size
+    #pyrosim.Send_Cube(name="Box", pos=[-2,2,.5], size=[1, 1, 1])
 
-#Store a box with specific position and size
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+    #Close sdf file
+    pyrosim.End()
 
-#Close sdf file
-pyrosim.End()
+#Create_Robot creates a robot
+def Create_Robot():
 
+    #Tell pyrosim where info about body of the robot should be stored
+    pyrosim.Start_URDF("body.urdf")
+    #Store Link0 with specific position and size
+    pyrosim.Send_Cube(name="s0", pos=[0,0,.5], size=[1, 1, 1], mass=0.0001)
+
+    #Joint to connect Link0 and Link1 and Torso
+    pyrosim.Send_Joint( name = "s0_s1", parent = "s0", child = "s1",
+                        type = "revolute", position = [0,0.5,0.5])
+    
+    #Store Link1 with specific position and size
+    pyrosim.Send_Cube(name="s1", pos=[0,.5,0], size=[1, 1, 1])
+    
+    pyrosim.End()
+
+    
+    
+Create_World()
+Create_Robot()
+
+
+
+
+    
--- simulate.py
+++ simulate.py
@@ -2,14 +2,27 @@
 #This program simulates the world robots will live in
 import pybullet as p
 import time
+import pybullet_data
 
 #Create objects that handles physics and draws results to GUI
 physicsClient = p.connect(p.GUI)
 
-#Reads in the world described in box.sdf
-p.loadSDF("box.sdf")
+#Create normal force generated when hitting floor
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-#The for loop is used to slow thingd down
+#Add force of gravity
+p.setGravity(0,0,-9.8)
+
+#Add floor
+planeId = p.loadURDF("plane.urdf")
+
+#Add robot
+robotId = p.loadURDF("body.urdf")
+
+#Reads in the world described in world.sdf
+p.loadSDF("world.sdf")
+
+#The for loop is used to slow things down
 for i in range(1000):
     #Steps physics inside the world
     p.stepSimulation()
